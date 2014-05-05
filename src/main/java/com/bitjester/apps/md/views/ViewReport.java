c97768fa2a051a85170143ad05fc730b773bc39a
luis - Gallia <luis@192.168.2.100>
2014-05-05 10:40:48 -0600

    Added Serializable to ViewReport.

--- src/main/java/com/bitjester/apps/md/views/ViewReport.java
+++ src/main/java/com/bitjester/apps/md/views/ViewReport.java
@@ -1,5 +1,7 @@
 package com.bitjester.apps.md.views;
 
+import java.io.Serializable;
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -9,7 +11,9 @@ import com.bitjester.apps.common.utils.BookKeeper;
 
 @Named
 @ViewScoped
-public class ViewReport {
+public class ViewReport implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Inject
 	private BookKeeper bk;
 
