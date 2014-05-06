76db8dd879d02d25c367da8211e124cba7d3945f
luis - Gallia <luis@192.168.2.100>
2014-05-05 19:48:24 -0600

    Added Search View.

---
+++ src/main/java/com/bitjester/apps/md/views/ViewSearch.java
@@ -0,0 +1,22 @@
+package com.bitjester.apps.md.views;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import com.bitjester.apps.common.utils.BookKeeper;
+
+@Named
+@ViewScoped
+public class ViewSearch implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private BookKeeper bk;
+
+	@Inject
+	private EntityManager em;
+}
